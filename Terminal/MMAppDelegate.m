a6be45ef2f59ec0b4e79b129e798a946a85e0d4f
Mehdi Mulani <mmmulani@uwaterloo.ca>
2013-05-16 16:21:08 -0400

    Use SMJobBless to run ProcessMonitor (untested).

--- Terminal/MMAppDelegate.m
+++ Terminal/MMAppDelegate.m
@@ -10,6 +10,9 @@
 #import "MMShared.h"
 #import "MMTerminalConnection.h"
 
+#import <ServiceManagement/ServiceManagement.h>
+#import <Security/Authorization.h>
+
 @implementation MMAppDelegate
 
 - (IBAction)createNewTerminal:(id)sender;
@@ -20,6 +23,22 @@
     MMTerminalConnection *terminalConnection = [[MMTerminalConnection alloc] initWithIdentifier:uniqueIdentifier];
     [self.terminalConnections addObject:terminalConnection];
     [terminalConnection createTerminalWindow];
+}
+
+- (void)startProcessMonitor;
+{
+    // In order for the process monitor to observer processes and determine information about them, it must be run as root.
+    NSString *programId = @"mmm.ProcessMonitor";
+    OSStatus status;
+    AuthorizationFlags authFlags = kAuthorizationFlagDefaults;
+    AuthorizationRef authRef;
+    status = AuthorizationCreate(NULL, kAuthorizationEmptyEnvironment, authFlags, &authRef);
+
+    AuthorizationItem authItem = { kSMRightBlessPrivilegedHelper, 0, NULL, 0 };
+	AuthorizationRights authRights = { 1, &authItem };
+    authFlags = kAuthorizationFlagDefaults | kAuthorizationFlagInteractionAllowed | kAuthorizationFlagPreAuthorize | kAuthorizationFlagExtendRights;
+    status = AuthorizationCopyRights(authRef, &authRights, kAuthorizationEmptyEnvironment, authFlags, NULL);
+    SMJobBless(kSMDomainSystemLaunchd, (__bridge CFStringRef)programId, authRef, NULL);
 }
 
 # pragma mark - NSApplicationDelegate
@@ -34,6 +53,8 @@
     [self.debugWindow showWindow:nil];
 
     [self createNewTerminal:nil];
+
+    [self startProcessMonitor];
 }
 
 - (void)_logMessage:(NSString *)message;
